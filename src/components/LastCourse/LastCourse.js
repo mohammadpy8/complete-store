bd7b510b402a7f301ff4453d64a63d9d8f514b3f
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-01 12:54:31 +0330

    courses-box

--- src/components/LastCourse/LastCourse.js
+++ src/components/LastCourse/LastCourse.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./LastCourse.css";
 import SectionHeader from "../SectionHeader/SectionHeader";
+import CourseBox from "../CourseBox/CourseBox";
 
 const LastCourse = () => {
   return (
@@ -12,6 +13,18 @@ const LastCourse = () => {
             desc="سکوی پرتاپ شما به سمت موفقیت"
             btnTitle="تمامی دوره ها"
           />
+          <div class="courses-content">
+            <div class="container">
+              <div class="row">
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
