f2ffea331decb66093556b3abb46beb4072cc38d
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-02 15:05:01 +0330

    breadcrumb

--- src/pages/CourseInfo/CourseInfo.js
+++ src/pages/CourseInfo/CourseInfo.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import TopBar from "../../components/TopBar/TopBar";
+import Footer from "../../components/Footer/Footer";
+import BreadCrumb from "../../components/BreadCrumb/BreadCrumb";
+
 import "./CourseInfo.css";
 
 const CourseInfo = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <>
+      <TopBar />
+      <Navbar />
+      <BreadCrumb
+        links={[
+          { id: 1, title: "خانه", to: "/" },
+          { id: 2, title: "آموزش برنامه نویسی فرانت اند", to: "/category-info/frontend" },
+          { id: 3, title: "دوره متخصص ریکت", to: "/course-info/js-expert" },
+        ]}
+      />
+      <Footer />
+    </>
+  );
 };
 
 export default CourseInfo;
