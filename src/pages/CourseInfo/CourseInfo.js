9e8f2d3f3123da896af93641f4b96d0859b4e9db
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-05 18:28:48 +0330

    details-section

--- src/pages/CourseInfo/CourseInfo.js
+++ src/pages/CourseInfo/CourseInfo.js
@@ -20,21 +20,25 @@ const CourseInfo = () => {
             title: "آموزش برنامه نویسی فرانت اند",
             to: "/category-info/frontend",
           },
-          { id: 3, title: "دوره متخصص ریکت و ریداکس", to: "/course-info/js-expert" },
+          {
+            id: 3,
+            title: "دوره متخصص ریکت و ریداکس",
+            to: "/course-info/js-expert",
+          },
         ]}
       />
 
-      <section class="course-info">
-        <div class="container">
-          <div class="row">
-            <div class="col-6">
-              <a href="#" class="course-info__link">
+      <section className="course-info">
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <a href="#" className="course-info__link">
                 آموزش برنامه نویسی فرانت اند
               </a>
-              <h1 class="course-info__title">
+              <h1 className="course-info__title">
                 آموزش 20 کتابخانه جاوااسکریپت برای بازار کار
               </h1>
-              <p class="course-info__text">
+              <p className="course-info__text">
                 امروزه کتابخانه‌ها کد نویسی را خیلی آسان و لذت بخش تر کرده اند.
                 به قدری که حتی امروزه هیچ شرکت برنامه نویسی پروژه های خود را با
                 Vanilla Js پیاده سازی نمی کند و همیشه از کتابخانه ها و فریمورک
@@ -42,24 +46,24 @@ const CourseInfo = () => {
                 عالی فرانت اند باشید، باید کتابخانه های کاربردی که در بازار کار
                 استفاده می شوند را به خوبی بلد باشید
               </p>
-              <div class="course-info__social-media">
-                <a href="#" class="course-info__social-media-item">
-                  <i class="fab fa-telegram-plane course-info__icon"></i>
+              <div className="course-info__social-media">
+                <a href="#" className="course-info__social-media-item">
+                  <i className="fab fa-telegram-plane course-info__icon"></i>
                 </a>
-                <a href="#" class="course-info__social-media-item">
-                  <i class="fab fa-twitter course-info__icon"></i>
+                <a href="#" className="course-info__social-media-item">
+                  <i className="fab fa-twitter course-info__icon"></i>
                 </a>
-                <a href="#" class="course-info__social-media-item">
-                  <i class="fab fa-facebook-f course-info__icon"></i>
+                <a href="#" className="course-info__social-media-item">
+                  <i className="fab fa-facebook-f course-info__icon"></i>
                 </a>
               </div>
             </div>
 
-            <div class="col-6">
+            <div className="col-6">
               <video
                 src=""
                 poster="/images/courses/js_project.png"
-                class="course-info__video"
+                className="course-info__video"
                 controls
               ></video>
             </div>
@@ -67,13 +71,13 @@ const CourseInfo = () => {
         </div>
       </section>
 
-      <main class="main">
-        <div class="container">
-          <div class="row">
-            <div class="col-8">
-              <div class="course">
-                <div class="course-boxes">
-                  <div class="row">
+      <main className="main">
+        <div className="container">
+          <div className="row">
+            <div className="col-8">
+              <div className="course">
+                <div className="course-boxes">
+                  <div className="row">
                     <CourseDetailBox
                       icon="graduation-cap"
                       title="وضعیت دوره"
@@ -99,7 +103,227 @@ const CourseInfo = () => {
                       title="پیش نیاز ها:"
                       text="HTML CSS"
                     />
-                    <CourseDetailBox icon="play" title="نوع مشاهده :" text="ضبط شده / آنلاین" />
+                    <CourseDetailBox
+                      icon="play"
+                      title="نوع مشاهده :"
+                      text="ضبط شده / آنلاین"
+                    />
+                  </div>
+                </div>
+                <div className="course-progress">
+                  <div className="course-progress__header">
+                    <i className="fas fa-chart-line course-progress__icon"></i>
+                    <span className="course-progress__title">
+                      درصد پیشرفت دوره: 75%
+                    </span>
+                  </div>
+                  <div className="progress course-progress__bar">
+                    <div
+                      className="progress-bar progress-bar-striped progress-bar-animated"
+                      role="progressbar"
+                      aria-label="Animated striped example"
+                      aria-valuenow="75"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                      style={{ width: "75%" }}
+                    ></div>
+                  </div>
+                </div>
+                <div className="introduction">
+                  <div className="introduction__item">
+                    <span className="introduction__title title">
+                      آموزش 20 کتابخانه جاوا اسکریپت مخصوص بازار کار
+                    </span>
+                    <img
+                      src="/images/info/1.gif"
+                      alt="course info image"
+                      className="introduction__img img-fluid"
+                    />
+                    <p className="introduction__text">
+                      کتابخانه های بسیار زیادی برای زبان جاوا اسکریپت وجود دارد
+                      و سالانه چندین کتابخانه جدید نیز به این لیست اضافه می شود
+                      که در بازار کار به شدت از آن ها استفاده می شود و اگر بدون
+                      بلد بودن این کتابخانه ها وارد بازار کار شوید، خیلی اذیت
+                      خواهید شد و حتی ممکن است ناامید شوید!
+                    </p>
+                    <p className="introduction__text">
+                      در این دوره نحوه کار با 20 مورد از پر استفاده ترین
+                      کتابخانه های جاوا اسکریپت به صورت پروژه محور به شما عزیزان
+                      آموزش داده می شود تا هیچ مشکلی برای ورود به بازار کار
+                      نداشته باشید
+                    </p>
+                  </div>
+                  <div className="introduction__item">
+                    <span className="introduction__title title">
+                      هدف از این دوره چیست؟ (تنها راه ورود به بازار کار و کسب
+                      درآمد)
+                    </span>
+                    <img
+                      src="/images/info/2.jpg"
+                      alt="course info image"
+                      className="introduction__img img-fluid"
+                    />
+                    <p className="introduction__text">
+                      وقتی برای اولین بار وارد یکی از شرکت های برنامه نویسی شدم،
+                      از کتابخانه هایی به اسم Lodash و Formik استفاده می شد، در
+                      حالی که من اولین بارم بود اسم Formik را می شنیدم و تا اون
+                      موقع از این کتابخانه ها استفاده نکرده بودم.
+                    </p>
+                    <p className="introduction__text">
+                      همینجا بود که متوجه شدم کتابخانه های جاوا اسکریپت یکی از
+                      مهم ترین مباحثی هستند که هر برنامه نویس وب برای ورود به
+                      بازار کار و کسب درآمد بهتر، راحت و بیشتر باید با آن ها کار
+                      کرده باشد{" "}
+                    </p>
+                    <p className="introduction__text">
+                      همان طور که از اسم این دوره مشخص است، هدف از این دوره
+                      آموزش 20 مورد از کاربردی ترین و پر استفاده ترین کتابخانه
+                      های جاوا اسکریپت است تا شما بتوانید بعد از این دوره با
+                      قدرت و آمادگی بیشتر ادامه مسیر برنامه نویسی وب را ادامه
+                      دهید، ری اکت یا نود یا … را راحت تر یاد بگیرید و در نهایت
+                      وارد بازار کار شده و کسب درآمد کنید.
+                    </p>
+                    <p className="introduction__text">
+                      شا به عنوان یک برنامه نویس وب، حداقل اگر با کتابخانه خاصی
+                      کار نکرده باشید، باید بلد باشید که چطور باید یک کتابخانه
+                      جدید را یاد بگیرید. فرض کنید یک یک کتابخانه جدید ساخته شد.
+                      آیا شما باید منتظر دوره آموزشی باشید؟! قطعا نه.
+                    </p>
+                    <p className="introduction__text">
+                      در این دوره سعی کردیم علاوه بر آموزش مستقیم هر کتابخانه،
+                      نحوه یادگیری یک کتابخانه جدید را نیز به شما عزیزان آموزش
+                      دهیم تا بعد از گذراندن دوره، دیگر وابسته هیچ دوره یا شخص
+                      خاصی نباشید و اگر کتابخانه جدیدی به دنیای جاوا اسکریپت و
+                      وب اضافه شد، به راحتی بتوانید آن را یاد بگیرید.
+                    </p>
+                  </div>
+                  <div className="introduction__btns">
+                    <a href="#" className="introduction__btns-item">
+                      دانلود همگانی ویدیوها
+                    </a>
+                    <a href="#" className="introduction__btns-item">
+                      دانلود همگانی پیوست‌ها
+                    </a>
+                  </div>
+
+                  <div className="introduction__topic">
+                    <div className="accordion" id="accordionExample">
+                      <div className="accordion-item">
+                        <h2 className="accordion-header" id="headingOne">
+                          <button
+                            className="accordion-button"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#collapseOne"
+                            aria-expanded="true"
+                            aria-controls="collapseOne"
+                          >
+                            معرفی دوره
+                          </button>
+                        </h2>
+                        <div
+                          id="collapseOne"
+                          className="accordion-collapse collapse show"
+                          aria-labelledby="headingOne"
+                          data-bs-parent="#accordionExample"
+                        >
+                          <div className="accordion-body introduction__accordion-body">
+                            <div className="introduction__accordion-right">
+                              <span className="introduction__accordion-count">
+                                1
+                              </span>
+                              <i className="fab fa-youtube introduction__accordion-icon"></i>
+                              <a href="#" className="introduction__accordion-link">
+                                معرفی دوره + چرا یادگیری کتابخانه ها ضروری است؟
+                              </a>
+                            </div>
+                            <div className="introduction__accordion-left">
+                              <span className="introduction__accordion-time">
+                                18:34
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="accordion" id="accordionExample2">
+                      <div className="accordion-item">
+                        <h2 className="accordion-header" id="headingTwo">
+                          <button
+                            className="accordion-button"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#collapseTwo"
+                            aria-expanded="true"
+                            aria-controls="collapseTwo"
+                          >
+                            معرفی دوره
+                          </button>
+                        </h2>
+                        <div
+                          id="collapseTwo"
+                          className="accordion-collapse collapse"
+                          aria-labelledby="headingTwo"
+                          data-bs-parent="#accordionExample2"
+                        >
+                          <div className="accordion-body introduction__accordion-body">
+                            <div className="introduction__accordion-right">
+                              <span className="introduction__accordion-count">
+                                1
+                              </span>
+                              <i className="fab fa-youtube introduction__accordion-icon"></i>
+                              <a href="#" className="introduction__accordion-link">
+                                معرفی دوره + چرا یادگیری کتابخانه ها ضروری است؟
+                              </a>
+                            </div>
+                            <div className="introduction__accordion-left">
+                              <span className="introduction__accordion-time">
+                                18:34
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="accordion" id="accordionExample3">
+                      <div className="accordion-item">
+                        <h2 className="accordion-header" id="headingThree">
+                          <button
+                            className="accordion-button"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#collapseThree"
+                            aria-expanded="true"
+                            aria-controls="collapseThree"
+                          >
+                            معرفی دوره
+                          </button>
+                        </h2>
+                        <div
+                          id="collapseThree"
+                          className="accordion-collapse collapse"
+                          aria-labelledby="headingThree"
+                          data-bs-parent="#accordionExample3"
+                        >
+                          <div className="accordion-body introduction__accordion-body">
+                            <div className="introduction__accordion-right">
+                              <span className="introduction__accordion-count">
+                                1
+                              </span>
+                              <i className="fab fa-youtube introduction__accordion-icon"></i>
+                              <a href="#" className="introduction__accordion-link">
+                                معرفی دوره + چرا یادگیری کتابخانه ها ضروری است؟
+                              </a>
+                            </div>
+                            <div className="introduction__accordion-left">
+                              <span className="introduction__accordion-time">
+                                18:34
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
