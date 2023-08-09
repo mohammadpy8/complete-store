fc7487617a8b178d6cb256fc0f5348e840649904
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-09 19:32:38 +0330

    courses-section

--- src/pages/Courses/Courses.js
+++ src/pages/Courses/Courses.js
@@ -18,16 +18,58 @@ const Courses = () => {
           { id: 1, title: "خانه", to: "/" },
           {
             id: 2,
-            title: "آموزش برنامه نویسی فرانت اند",
-            to: "/category-info/frontend",
-          },
-          {
-            id: 3,
-            title: "دوره متخصص ریکت و ریداکس",
-            to: "/course-info/js-expert",
+            title: "تمامی دوره ها",
+            to: "courses",
           },
         ]}
       />
+
+      <section class="courses">
+        <div class="container">
+          <div class="courses-content">
+            <div class="container">
+              <div class="row">
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+              </div>
+            </div>
+          </div>
+
+          <div class="courses-pagination">
+            <ul class="courses__pagination-list">
+              <li class="courses__pagination-item">
+                <a href="#" class="courses__pagination-link">
+                  <i class="fas fa-long-arrow-alt-right courses__pagination-icon"></i>
+                </a>
+              </li>
+              <li class="courses__pagination-item">
+                <a
+                  href="#"
+                  class="courses__pagination-link courses__pagination-link--active"
+                >
+                  1
+                </a>
+              </li>
+              <li class="courses__pagination-item">
+                <a href="#" class="courses__pagination-link">
+                  2
+                </a>
+              </li>
+              <li class="courses__pagination-item">
+                <a href="#" class="courses__pagination-link">
+                  3
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
 
       <Footer />
     </>
