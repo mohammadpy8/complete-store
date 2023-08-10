4ea4a35f4afe76fef44d8fbdda5ab40db53ac451
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-10 09:33:47 +0330

    form-reducer

--- src/components/Form/Input.js
+++ src/components/Form/Input.js
@@ -1,23 +1,46 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 import "./Input.css";
 
+const inputReducer = (state, action) => {
+
+    switch (action.type) {
+        case "CHANGE": {
+            return {
+                ...state,
+                value: action.value,
+                inValid: action.inValid,
+            }
+        }
+        default: {
+            return state
+        }
+    }
+}
+
 const Input = (props) => {
-    
-  const changeHandler = (event) => console.log(event.target.value);
+
+  const [mainInput, dispatch] = useReducer(inputReducer, {
+    value: "",
+    inValid: false,
+  });
+
+  const changeHandler = (event) => dispatch({type : "CHANGE", value: event.target.value, inValid: true});
 
   const element =
     props.element === "input" ? (
       <input
         type={props.type}
         placeholder={props.placeholder}
-        className={props.className}
+        className={`${props.className} ${mainInput.inValid ? "success" : "error"}`}
+        value={mainInput.value}
         onChange={changeHandler}
       />
     ) : (
       <textarea
         placeholder={props.placeholder}
-        className={props.className}
+        className={`${props.className} ${mainInput.inValid ? "success" : "error"}`}
+        value={mainInput.value}
         onChange={changeHandler}
       />
     );
